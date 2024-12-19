7ea04851258ef947dbf5eae5f9fbe2a6c0edd079
Jhonatan Alexis Ossa Osorio <jhoalex2525@hotmail.com>
2024-12-18 21:28:40 -0500

    otros scripts

--- 18122024script.js
+++ 18122024script.js
@@ -1,13 +1,25 @@
 const form = document.getElementById('userForm');
 const addButton = document.getElementById('addField');
+var nombre, correo, telefono, direccion;
 
+array = [ 'nombre', 'correo', 'telefono', 'direccion'];
+i = array.length;
+for(i=0; i<=4;i++){
 addButton.addEventListener('click', ()=> {
     const newField = document.createElement('input');
     newField.type = 'text';
     newField.name = 'extraField';
-    newField.placeholder = 'Additional Info';
+    newField.placeholder = array[i];
     form.appendChild(newField);
 });
-
+};
 const usernameInput = document.getElementById('username');
-usernameInput.addEventListener('input', ()=> {})
+usernameInput.addEventListener('input', ()=> {
+    const errorMessage = document.getElementById('error');
+    if(!usernameInput.value.match(/^[a-zA-Z0-9]{3,}$/)){
+        errorMessage.textContent = "Username must be at least 3 characters long and alphanumeric";
+    }else{
+        errorMessage.textContent ='';
+    }
+});
+
